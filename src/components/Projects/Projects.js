b8657ccbbfa86d975c9d8b67fa030bba811b5e98
SpicyJae <rlawogus9643@gmail.com>
2025-08-13 21:35:01 -0700

    Update projects for button css. Readme update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,6 +9,7 @@ import lcd from "../../assets/lcd_calculator.png";
 import portfolio from "../../assets/portfolio.png";
 import led_dimmer from "../../assets/LED_Dimmer.jpg";
 import pic3 from "../../assets/temp_profile.webp";
+import jlock from "../../assets/jlockbusters.png";
 import "../Projects/Projects.css";
 import Particle from "../Particle";
 import { Button } from "react-bootstrap";
@@ -39,24 +40,63 @@ function Projects() {
 
             >
                 <SwiperSlide>
-                    <div><img src={portfolio} />Jortfolio</div>
-                    <Link to="https://github.com/jh-kim0403/portfoliowebsite" target="_blank" rel="noopener noreferrer"><p><Button className="button">Github</Button></p></Link>
+                    <div className="project-card">
+                        <img src={portfolio} alt="Jortfolio" />
+                        <div className="project-title">Jortfolio</div>
+
+                        {/* Bottom-center, responsive actions */}
+                        <div className="cta">
+                            <Link to="https://www.jaeh-kim.com/" target="_blank" rel="noopener noreferrer">
+                                <Button className="cta-btn">Demo</Button>
+                            </Link>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div><img src={pic3} />JlockBusters</div>
-                    <Link to="https://github.com/jh-kim0403/fullstackProject" target="_blank" rel="noopener noreferrer"><p><Button className="button">Github</Button></p></Link>
+                    <div className="project-card">
+                        <img src={jlock} alt="Jlockbusters" />
+                        <div className="project-title">Jlockbusters</div>
+
+                        {/* Bottom-center, responsive actions */}
+                        <div className="cta">
+                            <Link to="https://jlockbusters.com" target="_blank" rel="noopener noreferrer">
+                                <Button className="cta-btn">Demo</Button>
+                            </Link>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div><img src={lcd} />LCD Calculator</div>
-                    <Link to="https://github.com/jh-kim0403/calculator" target="_blank" rel="noopener noreferrer"><p><Button className="button">Github</Button></p></Link>
+                    <div className="project-card">
+                        <img src={lcd} alt="LCD Calculator" />
+                        <div className="project-title">LCD Calculator</div>
+                        <div className="cta">
+                            <Link to="https://github.com/jh-kim0403/calculator" target="_blank" rel="noopener noreferrer">
+                                <Button className="cta-btn">Github</Button>
+                            </Link>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div><img src={led_dimmer} />LED Dimmer</div>
-                    <Link to="https://github.com/jh-kim0403/led_dimmer" target="_blank" rel="noopener noreferrer"><p><Button className="button">Github</Button></p></Link>
+                    <div className="project-card">
+                        <img src={led_dimmer} alt="LED Dimmer" />
+                        <div className="project-title">LED Dimmer</div>
+                        <div className="cta">
+                            <Link to="https://github.com/jh-kim0403/led_dimmer" target="_blank" rel="noopener noreferrer">
+                                <Button className="cta-btn">Github</Button>
+                            </Link>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div><img src={pic3} />Search Engine</div>
-                    <Link to="/about"><p><Button className="button">Github</Button></p></Link>
+                    <div className="project-card">
+                        <img src={pic3} alt="Search Engine" />
+                        <div className="project-title">Search Engine</div>
+                        <div className="cta">
+                            <Link to="https://github.com/jh-kim0403" target="_blank" rel="noopener noreferrer">
+                                <Button className="cta-btn">Github</Button>
+                            </Link>
+                        </div>
+                    </div>
                 </SwiperSlide>
             </Swiper>
         </div>
